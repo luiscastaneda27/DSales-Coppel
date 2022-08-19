8b0c4f2117b4ec5c3ac67ba49f1ba8bcfdf4a9fd
Luis Castaneda <lcastanedad@deloitte.com>
2022-08-19 12:49:40 -0600

    LC: Primer Deploy Sprint 1

---
+++ .sfdx/typings/lwc/PISYS_Color1.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/PISYS_Color1" {
+    var PISYS_Color1: string;
+    export default PISYS_Color1;
+}
---
+++ .sfdx/typings/lwc/PISYS_Color2.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/PISYS_Color2" {
+    var PISYS_Color2: string;
+    export default PISYS_Color2;
+}
