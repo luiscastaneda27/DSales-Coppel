06d4da5af76adf33c2b11d5448f00949ee775c78
Luis Castaneda <lcastanedad@deloitte.com>
2022-09-12 13:32:35 -0600

    LC: HU de Daniel, certificado y producto adquirido.

---
+++ .sfdx/typings/lwc/DSALESLogoInicioGEX.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/DSALESLogoInicioGEX" {
+    var DSALESLogoInicioGEX: string;
+    export default DSALESLogoInicioGEX;
+}
---
+++ .sfdx/typings/lwc/DSALES_LogoGarantiaExtendida.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/DSALES_LogoGarantiaExtendida" {
+    var DSALES_LogoGarantiaExtendida: string;
+    export default DSALES_LogoGarantiaExtendida;
+}
