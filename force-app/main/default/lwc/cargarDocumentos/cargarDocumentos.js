1c2995f07dd985282e854df949e534f4b6b4c9c8
Nelson <xnelsonvsx@hotmail.com>
2022-10-19 18:07:59 -0500

    Seguda parte

--- force-app/main/default/lwc/cargarDocumentos/cargarDocumentos.js
+++ force-app/main/default/lwc/cargarDocumentos/cargarDocumentos.js
@@ -1,15 +1,36 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement,wire, track} from "lwc";
 import { CloseActionScreenEvent } from "lightning/actions";
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import DSALES_TIPO_DE_ENDOSO__C from '@salesforce/schema/DSALES_Endoso__c.DSALES_Tipodeendoso__c';
 
 
 export default class QuickActionLWC extends LightningElement {
-    @api
-    myRecordId;
-
-    get acceptedFormats() {
-        return ['.pdf', '.png'];
+    
+    @track pickListValues;
+    @track error;
+    @track values;
+    
+    @wire(getPicklistValues, { 
+        recordTypeId: '012000000000000AAA', 
+        fieldApiName: DSALES_TIPO_DE_ENDOSO__C 
+    })
+   
+    wiredPickListValue({data, error}){
+        if(data){
+            console.log(`pickList values are `,data.values);
+            this.pickListValues= data.values;
+            this.error= undefined;
+        }
+        if(error){
+            console.log(`Error while fetching the pickList values ${error}`);
+            this.pickListValues= undefined;
+            this.error= error;
+        }
     }
 
+    get acceptedFormats() {
+        return ['.pdf', '.png', '.xlsx', '.xls', '.csv', '.doc', '.docx'];
+    }
     handleUploadFinished(event) {
         // Get the list of uploaded files
         const uploadedFiles = event.detail.files;
@@ -20,7 +41,9 @@ export default class QuickActionLWC extends LightningElement {
     }
     handleSave = event =>{
         event.preventDefault();
-    }
+    } 
+    handleChange() {
        
+    }     
     
 }
