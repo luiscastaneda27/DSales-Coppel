0620b465e49547321979fcc992b5e20517869e93
Omar <ovg77ovg@gmail.com>
2022-10-21 10:49:00 -0600

    clasificacionservi Deploy ID: 0Af7500000EXHKKCA5

--- .sfdx/typings/lwc/apex/DSALES_EndorsementController.d.ts
+++ .sfdx/typings/lwc/apex/DSALES_EndorsementController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/DSALES_EndorsementController.crearEndoso" {
   export default function crearEndoso(param: {idEndoso: any}): Promise<any>;
 }
+declare module "@salesforce/apex/DSALES_EndorsementController.validarDoc" {
+  export default function validarDoc(param: {idObject: any}): Promise<any>;
+}
