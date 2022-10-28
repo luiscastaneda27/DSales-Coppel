a03e249cd93bc61667463f1f73fcd11fc6a38652
Sayra Martinez <sayra.martinez.3011@gmail.com>
2022-10-28 15:46:57 -0500

    lwc-endoso/siniestro

--- force-app/main/default/lwc/dSALES_SiniestroButton/dSALES_SiniestroButton.js
+++ force-app/main/default/lwc/dSALES_SiniestroButton/dSALES_SiniestroButton.js
@@ -2,6 +2,8 @@ import { LightningElement, track, api } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import crearSiniestro from '@salesforce/apex/DSALES_SiniestroController.Siniestro';  
+import validateDocs from '@salesforce/apex/DSALES_SiniestroController.validarDoc';  
+
 
 export default class dSALES_SiniestroButton extends LightningElement {
     @api recordId
@@ -11,17 +13,53 @@ export default class dSALES_SiniestroButton extends LightningElement {
       this.dispatchEvent(closeQA);
     }
   
-    sendSiniestro(){
-        crearSiniestro({idSiniestro: this.recordId})
-        .then(result => {
-            this.resul = result;
-            this.pushMessage('Exitoso', 'success', 'Siniestro Enviado exitosamente.');
-            eval("$A.get('e.force:refreshView').fire();");
-        }).catch(error => {
-            console.log('Error en enviar Siniestro');
-            console.log(this.estatus);
-            this.pushMessage('Error', 'error', 'Ha ocurrido un error, por favor contacte su administrador.');
-        });
+    sendSiniestro() {
+
+        validateDocs({ idObject: this.recordId })
+            .then(result => {
+                this.resul = result;
+                console.log(result);
+                if (result >= 18) {
+                    crearSiniestro({ idSiniestro: this.recordId })
+                        .then(result => {
+                            this.resul = result;
+                            console.log(result);
+                            this.pushMessage('Exitoso', 'success', 'Siniestro Enviado exitosamente.');
+                            eval("$A.get('e.force:refreshView').fire();");
+
+                        }).catch(error => {
+                            console.log('Error en enviar Siniestro');
+                            console.log(this.estatus);
+                            this.pushMessage('Error', 'error', 'Ha ocurrido un error, por favor contacte su administrador.');
+                        });
+                
+
+            }else if (result < 18) {
+                this.pushMessage('Error', 'error', 'Faltan documentos por subir');
+            }
+
+
+    }).catch(error => {
+        console.log('Error en enviar Siniestro');
+console.log(this.estatus);
+this.pushMessage('Error', 'error', 'Ha ocurrido un error, por favor contacte su administrador.');
+            });
+
+    }
+
+    crearSiniestroMain() {
+        crearSiniestro({ idSiniestro: this.recordId })
+            .then(result => {
+                this.resul = result;
+                console.log(result);
+                this.pushMessage('Exitoso', 'success', 'Siniestro Enviado exitosamente.');
+                eval("$A.get('e.force:refreshView').fire();");
+
+            }).catch(error => {
+                console.log('Error en enviar Siniestro');
+                console.log(this.estatus);
+                this.pushMessage('Error', 'error', 'Ha ocurrido un error, por favor contacte su administrador.');
+            });
     }
 
     pushMessage(title,variant,msj){
