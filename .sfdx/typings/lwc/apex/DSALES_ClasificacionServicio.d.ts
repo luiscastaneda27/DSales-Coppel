de12ced20078e877d26936bd4026dfde0f476c43
Sayra Martinez <sayra.martinez.3011@gmail.com>
2022-12-12 09:54:18 -0600

    omitir

--- .sfdx/typings/lwc/apex/DSALES_ClasificacionServicio.d.ts
+++ .sfdx/typings/lwc/apex/DSALES_ClasificacionServicio.d.ts
@@ -7,9 +7,72 @@ declare module "@salesforce/apex/DSALES_ClasificacionServicio.getRecords" {
 declare module "@salesforce/apex/DSALES_ClasificacionServicio.getBuscarSKU" {
   export default function getBuscarSKU(param: {sku: any}): Promise<any>;
 }
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getBuscarVinculacion" {
+  export default function getBuscarVinculacion(param: {servicio: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getidservicio" {
+  export default function getidservicio(param: {sku: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getBuscarProducto" {
+  export default function getBuscarProducto(param: {servicio: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.upsertVinculacion" {
+  export default function upsertVinculacion(param: {dataJSON: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.insertVinculacion" {
+  export default function insertVinculacion(param: {dataJSON: any, idservicio: any}): Promise<any>;
+}
 declare module "@salesforce/apex/DSALES_ClasificacionServicio.upsertRecord" {
   export default function upsertRecord(param: {allData: any}): Promise<any>;
 }
 declare module "@salesforce/apex/DSALES_ClasificacionServicio.getPicklistOptionsDependent" {
   export default function getPicklistOptionsDependent(param: {allData: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.checkProfileType" {
+  export default function checkProfileType(param: {profile: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getcategories" {
+  export default function getcategories(param: {recordName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getSubCategories" {
+  export default function getSubCategories(param: {valueCategoria: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getClases" {
+  export default function getClases(param: {valueCategoria: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getFamilias" {
+  export default function getFamilias(param: {valueClases: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getMatriz" {
+  export default function getMatriz(): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.updateMatriz" {
+  export default function updateMatriz(param: {allData: any, typeServicio: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.insertListaPrecios" {
+  export default function insertListaPrecios(param: {idproductoservicio: any, opcion: any, JSON2: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getPickListValuesIntoList1" {
+  export default function getPickListValuesIntoList1(): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getPickListValuesIntoList2" {
+  export default function getPickListValuesIntoList2(): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getPickListValuesIntoList3" {
+  export default function getPickListValuesIntoList3(): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.getPickListValuesIntoList4" {
+  export default function getPickListValuesIntoList4(): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.createProductIntan" {
+  export default function createProductIntan(param: {productIntan: any, sku: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.RecordTypeId" {
+  export default function RecordTypeId(param: {tipoRegistro: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.insertPocentajeCobro" {
+  export default function insertPocentajeCobro(param: {idservicio: any, JSONP: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DSALES_ClasificacionServicio.createVinculacion" {
+  export default function createVinculacion(param: {asignacion: any}): Promise<any>;
 }
