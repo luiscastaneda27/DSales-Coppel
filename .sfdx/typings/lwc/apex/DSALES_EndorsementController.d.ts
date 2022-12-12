dbd7c10277fc9103bd219e2d2590e6fddfa04246
luiscastaneda27 <68239429+luiscastaneda27@users.noreply.github.com>
2022-12-12 14:21:46 -0600

    Delete DSALES_EndorsementController.d.ts

--- .sfdx/typings/lwc/apex/DSALES_EndorsementController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/DSALES_EndorsementController.crearEndoso" {
-  export default function crearEndoso(param: {idEndoso: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DSALES_EndorsementController.validarDoc" {
-  export default function validarDoc(param: {idObject: any}): Promise<any>;
-}
